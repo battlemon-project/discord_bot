e7b563cf06b6335b9f06cef12c782dcadf364e0c
fomotoshi <fomotoshi@ya.ru>
2022-02-14 12:56:36 +0800

    initial commit

---
+++ battlemon_discord_bot/__init__.py
@@ -0,0 +1,27 @@
+__version__ = '0.1.0'
+
+import discord
+
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print('Logged in as')
+        print(self.user.name)
+        print(self.user.id)
+        print('------')
+
+    async def on_message(self, message):
+        # we do not want the bot to reply to itself
+        if message.author.id == self.user.id:
+            return
+
+        if message.content.startswith('!earyaccess'):
+            embed = discord.Embed(title="sign in",
+                                  url="https://wallet.testnet.near.org/login/?success_url=telegram%3A%2F%2F&failure_url=https%3A%2F%2Fdemo.battlemon.com%2F&contract_id=dev-1639312006189-41768496072291&public_key=ed25519%3AEzkpaH99JAxz2VAZCxcKo9Khs8b7GS4J5M2AcEShySMF",
+                                  description="sign in testnetwork",
+                                  color=0xFF5733)
+            await message.channel.send(embed=embed)
+
+
+client = MyClient()
+client.run('OTQyMDU3MTY1Nzg1NTMwNDE5.Yge9Gg.RkcyC6Hp7UywQVhpsqdDkWx3Kw8')
